a19f3ce9d9aec60abca66a229e749b4d2fc02850
Aaron <aaronde314@gmail.com>
2016-05-16 10:17:20 -0400

    worked on making options do stuff

--- app/src/main/java/menu/catz/aaron/catzmain/JSONLoader.java
+++ app/src/main/java/menu/catz/aaron/catzmain/JSONLoader.java
@@ -3,18 +3,13 @@ package menu.catz.aaron.catzmain;
 import android.content.Context;
 import android.support.annotation.Nullable;
 import android.util.Log;
-
 import java.io.IOException;
 import java.io.InputStream;
 
 public class JSONLoader {
     //http://stackoverflow.com/questions/30232051/read-json-file-from-assets
-    public static String getJson(Context context, String json){
-        return parseFileToString(context, json);
-    }
     @Nullable
-    public static String parseFileToString(Context context, String filename )
-    {
+    public static String parseFileToString(Context context, String filename ) {
         try
         {
             InputStream stream = context.getAssets().open( filename );
@@ -31,4 +26,5 @@ public class JSONLoader {
         }
         return null;
     }
+
 }
